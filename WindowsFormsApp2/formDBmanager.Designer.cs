13f0ad3c923aac6ffc1ff394720e916636d4a33d
DONGHEE <veststar1@gmail.com>
2024-01-10 16:52:13 +0900

    FILE DB 생성 후, SQL 쿼리 처리하는 다이얼로그 생성

--- WindowsFormsApp2/formDBmanager.Designer.cs
+++ WindowsFormsApp2/formDBmanager.Designer.cs
@@ -29,35 +29,211 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.편집ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.보기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.도움말ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuAbout = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sbLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tbSQL = new System.Windows.Forms.TextBox();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SQLaction = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.편집ToolStripMenuItem,
+            this.보기ToolStripMenuItem,
+            this.도움말ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(866, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuNew,
+            this.menuOpen,
+            this.menuSave,
+            this.toolStripSeparator1});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // menuNew
+            // 
+            this.menuNew.Name = "menuNew";
+            this.menuNew.Size = new System.Drawing.Size(180, 22);
+            this.menuNew.Text = "NEW";
+            // 
+            // menuOpen
+            // 
+            this.menuOpen.Name = "menuOpen";
+            this.menuOpen.Size = new System.Drawing.Size(180, 22);
+            this.menuOpen.Text = "OPEN";
+            this.menuOpen.Click += new System.EventHandler(this.menuOpen_Click);
+            // 
+            // menuSave
+            // 
+            this.menuSave.Name = "menuSave";
+            this.menuSave.Size = new System.Drawing.Size(180, 22);
+            this.menuSave.Text = "SAVE";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // 편집ToolStripMenuItem
+            // 
+            this.편집ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SQLaction});
+            this.편집ToolStripMenuItem.Name = "편집ToolStripMenuItem";
+            this.편집ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.편집ToolStripMenuItem.Text = "동작";
+            // 
+            // 보기ToolStripMenuItem
+            // 
+            this.보기ToolStripMenuItem.Name = "보기ToolStripMenuItem";
+            this.보기ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.보기ToolStripMenuItem.Text = "보기";
+            // 
+            // 도움말ToolStripMenuItem
+            // 
+            this.도움말ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuAbout});
+            this.도움말ToolStripMenuItem.Name = "도움말ToolStripMenuItem";
+            this.도움말ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.도움말ToolStripMenuItem.Text = "도움말";
+            // 
+            // menuAbout
+            // 
+            this.menuAbout.Name = "menuAbout";
+            this.menuAbout.Size = new System.Drawing.Size(112, 22);
+            this.menuAbout.Text = "ABOUT";
+            // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sbLabel1,
+            this.sbLabel2,
+            this.sbLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 482);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(866, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sbLabel1
+            // 
+            this.sbLabel1.Name = "sbLabel1";
+            this.sbLabel1.Size = new System.Drawing.Size(10, 17);
+            this.sbLabel1.Text = ".";
+            // 
+            // sbLabel2
+            // 
+            this.sbLabel2.Name = "sbLabel2";
+            this.sbLabel2.Size = new System.Drawing.Size(10, 17);
+            this.sbLabel2.Text = ".";
+            // 
+            // sbLabel3
+            // 
+            this.sbLabel3.Name = "sbLabel3";
+            this.sbLabel3.Size = new System.Drawing.Size(10, 17);
+            this.sbLabel3.Text = ".";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tbSQL);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataView);
+            this.splitContainer1.Size = new System.Drawing.Size(866, 458);
+            this.splitContainer1.SplitterDistance = 109;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // tbSQL
+            // 
+            this.tbSQL.AcceptsReturn = true;
+            this.tbSQL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbSQL.Location = new System.Drawing.Point(0, 0);
+            this.tbSQL.Multiline = true;
+            this.tbSQL.Name = "tbSQL";
+            this.tbSQL.Size = new System.Drawing.Size(866, 109);
+            this.tbSQL.TabIndex = 0;
+            // 
+            // dataView
+            // 
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataView.Location = new System.Drawing.Point(0, 0);
+            this.dataView.Name = "dataView";
+            this.dataView.RowTemplate.Height = 23;
+            this.dataView.Size = new System.Drawing.Size(866, 345);
+            this.dataView.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "MDF파일|*.mdf";
+            this.openFileDialog1.ValidateNames = false;
+            // 
+            // SQLaction
+            // 
+            this.SQLaction.Name = "SQLaction";
+            this.SQLaction.Size = new System.Drawing.Size(180, 22);
+            this.SQLaction.Text = "SQL실행";
+            this.SQLaction.Click += new System.EventHandler(this.SQLaction_Click);
             // 
             // formDBmanager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(866, 504);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "formDBmanager";
             this.Text = "DB manager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +243,23 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuNew;
+        private System.Windows.Forms.ToolStripMenuItem menuOpen;
+        private System.Windows.Forms.ToolStripMenuItem menuSave;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 편집ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 보기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 도움말ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuAbout;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel3;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox tbSQL;
+        private System.Windows.Forms.DataGridView dataView;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem SQLaction;
     }
 }
 
--- WindowsFormsApp2/formDBmanager.cs
+++ WindowsFormsApp2/formDBmanager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,138 @@ namespace WindowsFormsApp2
         public formDBmanager()
         {
             InitializeComponent();
+        }
+
+        string GetFileName(string sPath)
+        {
+            string[] sa = sPath.Split('\\');
+            return sa[sa.Length - 1];   
+        }
+
+
+        string sConn;
+        SqlConnection sqlConnect = new SqlConnection();
+        SqlCommand sqlCommand = new SqlCommand();   
+        private void menuOpen_Click(object sender, EventArgs e)
+        {
+            //DB SELECT
+            openFileDialog1.Filter = "MS-SQL Database file|*.mdf";
+            if (DialogResult.OK == openFileDialog1.ShowDialog()) {
+                //string sconn = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\EMBEDDED\\source\\repos\\winapp\\WinFormCS\\WindowsFormsApp2\\myDB.mdf;Integrated Security=True;Connect Timeout=30";
+                //string sconn = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\EMBEDDED\source\repos\winapp\WinFormCS\WindowsFormsApp2\myDB.mdf;Integrated Security=True;Connect Timeout=30";
+                //경로 저장
+                sConn = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={openFileDialog1.FileName};Integrated Security=True;Connect Timeout=30";
+                sqlConnect.ConnectionString = sConn;
+                
+                sqlConnect.Open();
+                sqlCommand.Connection = sqlConnect;
+                sbLabel1.Text = GetFileName(openFileDialog1.FileName);
+                sbLabel1.BackColor = Color.DarkOliveGreen;
+            }
+
+
+        }
+
+        public string[] RunSql(string sql)
+        {
+            sqlCommand.CommandText = sql;
+            if (sql.Trim().ToLower().Substring(0, 6) == "select")
+            {
+                SqlDataReader sr = sqlCommand.ExecuteReader();
+
+                //열의 갯수만큼 반복함 
+                for (int i = 0; i < sr.FieldCount; i++)
+                {
+                    string colName = sr.GetName(i);
+
+                    //추가되는 컬럼의 프로그래밍상 접근 이름, 표시되는 이름 
+                    //데이터뷰에 컬럼 생성
+                    dataView.Columns.Add(colName, colName);
+                }
+            }
+
+            //sr에는 sql 커맨드로 보여지는 데이터들이 저장되어 있음
+            SqlDataReader sr = sqlCommand.ExecuteReader();
+
+            //열의 갯수만큼 반복함 
+            for (int i = 0; i < sr.FieldCount; i++)
+            {
+                string colName = sr.GetName(i);
+
+                //추가되는 컬럼의 프로그래밍상 접근 이름, 표시되는 이름 
+                //데이터뷰에 컬럼 생성
+                dataView.Columns.Add(colName, colName);
+            }
+
+            for (; sr.Read();)
+            { //한줄씩 읽어서 처리를 해라 
+                string str = "";
+                int nRow = dataView.Rows.Add(); //row1개 추가, row가 몇개 생성됐는지 return     
+                for (int i = 0; i < sr.FieldCount; i++)
+                {
+                    //sr로부터 i번째 col 의 값을 가져오고 
+                    object obj = sr.GetValue(i);
+
+                    //dataview 어디에 저장할지 설정
+                    dataView.Rows[nRow].Cells[i].Value = obj;
+
+                    //첫번째 열의 경우 
+                    if (i == 0)
+                    {
+                        str += $"{obj}";
+                    }
+                    else
+                    {//2번째 이상 컬럼의 경우 
+                        str += $",{obj}";
+                    }
+
+
+                }
+
+            }
+        }
+
+
+        private void SQLaction_Click(object sender, EventArgs e)
+        {
+            string sql = "Select * from person";
+            sqlCommand.CommandText = sql;
+
+            //sr에는 sql 커맨드로 보여지는 데이터들이 저장되어 있음
+            SqlDataReader sr = sqlCommand.ExecuteReader();
+
+            //열의 갯수만큼 반복함 
+            for(int i =0; i <sr.FieldCount; i++) {
+                string colName = sr.GetName(i);
+
+                //추가되는 컬럼의 프로그래밍상 접근 이름, 표시되는 이름 
+                //데이터뷰에 컬럼 생성
+                dataView.Columns.Add(colName, colName);
+            }
+
+            for(;sr.Read(); ) { //한줄씩 읽어서 처리를 해라 
+                string str = "";
+                 int nRow = dataView.Rows.Add(); //row1개 추가, row가 몇개 생성됐는지 return     
+                for(int i = 0; i  < sr.FieldCount; i++) {
+                    //sr로부터 i번째 col 의 값을 가져오고 
+                    object obj = sr.GetValue(i);
+
+                    //dataview 어디에 저장할지 설정
+                    dataView.Rows[nRow].Cells[i].Value = obj;   
+
+                    //첫번째 열의 경우 
+                    if (i == 0)
+                    {
+                        str += $"{obj}";
+                    }
+                    else {//2번째 이상 컬럼의 경우 
+                        str += $",{obj}";
+                    }
+
+                    
+                }
+                tbSQL.Text += str +"\r\n";   
+            }
         }
     }
 }
